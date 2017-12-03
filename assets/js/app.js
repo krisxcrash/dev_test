975c0aa0fa14761e298a9b28eb948586bb76b8d6
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-03 15:11:58 -0800

    updated js - added filter by product section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,110 +1,60 @@
 var topics = [];
-
 //runs when the page is ready
-$(document).on('click', '#searchButton', function() {
 
-})
-$(document).ready(function() {
 
-    renderButtons();
-	
-// function to query the gifs on button click
-	
-	function bindClick(){
+var queryURL = "https://www.wirelessemporium.com/products.json";
+$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response) {
+		console.log(response);
+		console.log(response.products[0].id);
+		var results = response.products;
+		for (var i = 0; i < results.length; i++) {
+			var productsDiv = $("<div class='item col-md-6 col-sm-6 col-xs-12 squares'>");
+            var description = results[i].title;
+            var productType = results[i].product_type;
+            var productVariant = results[i].variants;
 
-	$("button").on("click", function() {
+            for (let j = 0; j < productVariant.length; j++) {
+                const variants = productVariant[j];
+            }
+            console.log(productType)
+                var cat = $("<p class='cat'>").text(productType);
+                var h4 = $("<h4 class='product-info'>")
+                .text(description);
+                var h6 = $("<h6 class='product-info'>")
+				.text(description);
+                var productImage = $("<img>");
+                productImage.attr("src", results[i].images[0].src);
+                productImage.attr("height", "400");
+                productImage.attr("width", "400")
+                productImage.addClass("products-image");
+                
+                productsDiv.prepend(h4);
+                productsDiv.prepend(cat);
+                productsDiv.prepend(productImage);
+                $("#resultsHere")
+                    .prepend(productsDiv);
 
-// assigns attribute to button for gif query
-        
-	var gifButton = $(this).attr("data-person");
-		
-		if(gifButton !== undefined) {
-			var q = gifButton;
-		}
-		else{
-			q = $("#searchInput").val();
-		}		
-		
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
 
-	    $.ajax({
-	    	url: queryURL,
-	    	method: "GET"
-	    })
-	    .done(function(response) {
-	    	var results = response.data;
-
-	    	for (var i = 0; i < results.length; i++) {
-	    		var gifDiv = $("<div class='item col-md-4 col-sm-6 squares'>");
-
-	    		var rating = results[i].rating;
-
-	    		var p = $("<p>").text("Rating: " + rating);
-
-	    		var gifImage = $("<img>");
-	    		gifImage.attr("src", results[i].images.fixed_width_still.url);
-	    		gifImage.attr("data-still", results[i].images.fixed_width_still.url);
-	    		gifImage.attr("data-animate", results[i].images.fixed_width.url);
-	    		gifImage.attr("data-state", "still");
-	    		gifImage.attr("height", "300");
-	    		gifImage.attr("width", "400")
-	    		gifImage.addClass("gif");
-
-	    		gifDiv.prepend(p);
-	    		gifDiv.prepend(gifImage);
-
-	    		$("#gifsHere").prepend(gifDiv);
-	    	};
-
-//if statement to pause and play gifs
-
-		    $(".gif").on("click", function() {
-
-				var state = $(this).attr("data-state");
-		    	var animate = $(this).attr("data-animate");
-		    	var still = $(this).attr("data-still");
-
-			    if (state === "still") {
-			    	$(this).attr("src", animate);
-			    	$(this).attr("data-state", "animate");
-			      }
-			    else {
-			    	$(this).attr("src", still);
-			    	$(this).attr("data-state", "still");
-			    };
-			});
-	    });
-	});
-};
-	
-// function to add buttons when user searches
-
-    function renderButtons() {
-    	$("#buttons").empty();
-
-		for (var i = 0; i < topics.length; i++) {
-
-			var a = $("<button>");
-			a.addClass("btn btn-default btn-lg btnDesign giphyButton");
-			a.attr("data-person", topics[i]);
-			a.text(topics[i]);
-
-			$("#buttons").append(a);
-		}
-
-		bindClick();
-    }
-
-// adds search item to array
-
-    $("#searchButton").on("click", function(event) {
-    	event.preventDefault();
-
-    	var gifButton = $("#searchInput").val().trim();
-
-    	topics.push(gifButton);
-
-    	renderButtons();
+		};
     });
 
-});    
+$(document).on('click', '#searchButton', function() {
+    var title = "11384648583";
+    var queryURL = "https://www.wirelessemporium.com/products.json";
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(response) {
+            console.log(response);
+            console.log(response.products[0].id);
+            var results = response.products;
+            for (var i = 0; i < results.length; i++) {
+            }
+
+})
+});
