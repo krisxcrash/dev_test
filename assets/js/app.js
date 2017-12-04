31f1a9da96671c1f45716b18d9c8240e74c509c1
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-04 11:45:48 -0800

    updated layout for filter results

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,7 @@ $.ajax( {
 				.text( productType );
 			var h4 = $( "<h4 class='product-info'>" )
 				.text( description );
-			var h6 = $( "<h6 class='product-info'>" )
-				.text( description );
+			
 			var productImage = $( "<img>" );
 			productImage.attr( "src", results[ i ].images[ 0 ].src );
 			productImage.attr( "height", "400" );
@@ -72,7 +71,7 @@ function categoryFilter() {
 				.done( function( response ) {
 					var results = response.products;
 					for ( var i = 0; i < results.length; i++ ) {
-						var productsDiv = $( "<div class='item col-md-6 col-sm-6 col-xs-12 squares'>" );
+						var productsDiv = $( "<div class='item col-md-6 col-sm-6 col-xs-12 squares clearfix'>" );
 						var description = results[ i ].title;
 						var productType = results[ i ].product_type;
 						var productVariant = results[ i ].variants;
@@ -84,8 +83,7 @@ function categoryFilter() {
 								.text( productType );
 							var h4 = $( "<h4 class='product-info'>" )
 								.text( description );
-							var h6 = $( "<h6 class='product-info'>" )
-								.text( description );
+
 							var productImage = $( "<img>" );
 							productImage.attr( "src", results[ i ].images[ 0 ].src );
 							productImage.attr( "height", "400" );
