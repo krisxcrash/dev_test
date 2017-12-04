d3c7c7d2397ec28ac8818c4f11a4695fc030007a
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-03 18:21:38 -0800

    updated js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 
+// console.log(results[0].variants[0].price);
+
 var queryURL = "https://www.wirelessemporium.com/products.json";
 $.ajax({
 		url: queryURL,
 		method: "GET"
 	})
 	.done(function(response) {
-		console.log(response);
-		console.log(response.products[0].id);
+
 		var results = response.products;
 		for (var i = 0; i < results.length; i++) {
 			var productsDiv = $("<div class='item col-md-6 col-sm-6 col-xs-12 squares'>");
@@ -14,11 +15,11 @@ $.ajax({
             var productType = results[i].product_type;
             var productVariant = results[i].variants;
 
-            for (let j = 0; j < productVariant.length; j++) {
-                const variants = productVariant[j];
+            for (var j = 0; j < productVariant.length; j++) {
+                var variants = productVariant[j];
             }
 
-            console.log(productType);
+            console.log(variants);
                 var cat = $("<p class='cat'>").text(productType);
                 var h4 = $("<h4 class='product-info'>")
                 .text(description);
@@ -63,24 +64,25 @@ var categoryList = ["Cases & Covers",
             
             $("#product-list").append(list);
         }
-        bindClick();
+        categoryFilter();
     }
 
-    function bindClick(){
-        var filterList = $(this).attr("value");
+    function categoryFilter(){
+        $("li").on("click", function() {
 
-        if (filterList !== undefined) {
-            
-            $("#resultsHere").addClass("hidden");
-            
+
+        var filterList = $(this).attr("value");
+        
+        console.log(filterList);
+        $("#filter-results").text("hola");
+
                 var queryURL = "https://www.wirelessemporium.com/products.json";
                 $.ajax({
                         url: queryURL,
                         method: "GET"
                     })
                     .done(function(response) {
-                        console.log(response);
-                        console.log(response.products[0].id);
+
                         var results = response.products;
                         for (var i = 0; i < results.length; i++) {
                             var productsDiv = $("<div class='item col-md-6 col-sm-6 col-xs-12 squares'>");
@@ -88,11 +90,13 @@ var categoryList = ["Cases & Covers",
                             var productType = results[i].product_type;
                             var productVariant = results[i].variants;
                 
-                            for (let j = 0; j < productVariant.length; j++) {
-                                const variants = productVariant[j];
+                            for (var j = 0; j < productVariant.length; j++) {
+                                var variants = productVariant[j];
                             }
             
-                            if (catFilter.value === productType) {
+                            if (filterList.value === productType[i]) {
+
+                                
                                 var cat = $("<p class='cat'>").text(productType);
                                 var h4 = $("<h4 class='product-info'>")
                                 .text(description);
@@ -114,60 +118,17 @@ var categoryList = ["Cases & Covers",
                 
                 
                         };
+
+                        $("#resultsHere").addClass("hidden");
+                        $("#filter-results").removeClass("hidden");
                     });
-            
+    })
+};
 
-
-        }
-    }
-
-// $(document).on("click", ".product-list", function() {
-//     var catFilter = $(this).value;
-
-//     $("#resultsHere").addClass("hidden");
-
-//     var queryURL = "https://www.wirelessemporium.com/products.json";
-//     $.ajax({
-//             url: queryURL,
-//             method: "GET"
-//         })
-//         .done(function(response) {
-//             console.log(response);
-//             console.log(response.products[0].id);
-//             var results = response.products;
-//             for (var i = 0; i < results.length; i++) {
-//                 var productsDiv = $("<div class='item col-md-6 col-sm-6 col-xs-12 squares'>");
-//                 var description = results[i].title;
-//                 var productType = results[i].product_type;
-//                 var productVariant = results[i].variants;
-    
-//                 for (let j = 0; j < productVariant.length; j++) {
-//                     const variants = productVariant[j];
-//                 }
-
-//                 if (catFilter.value === productType) {
-//                     var cat = $("<p class='cat'>").text(productType);
-//                     var h4 = $("<h4 class='product-info'>")
-//                     .text(description);
-//                     var h6 = $("<h6 class='product-info'>")
-//                     .text(description);
-//                     var productImage = $("<img>");
-//                     productImage.attr("src", results[i].images[0].src);
-//                     productImage.attr("height", "400");
-//                     productImage.attr("width", "400")
-//                     productImage.addClass("products-image");
-                    
-//                     productsDiv.prepend(h4);
-//                     productsDiv.prepend(cat);
-//                     productsDiv.prepend(productImage);
-//                     $("#filter-results")
-//                         .prepend(productsDiv);
-//                 }
-                    
-    
-    
-//             };
-//         });
-
-// })
-    
+function sortPrice() {
+    var res = $.grep(data.items, function(v) {
+        return name.indexOf(v.Name) > -1; //return all columns contained in the list that are also in the name array
+      });
+      
+      document.write('<pre>' + JSON.stringify(res, 0, 3) + '</pre>');
+}
