b23d8a529d34f3275c50d12c984f3cc48d1b25e9
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-04 19:28:34 -0800

    updated sort by price - pushing nested prices into the array

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,27 +106,30 @@ function categoryFilter() {
 
 
 
-    $( ".price-sort" )
-	.on( "click", function() {
-		$( "#filter-results" )
-			.empty(); //clears results from previous click
 
-		event.preventDefault(); //prevents window from reloading on click
-		var priceSort = $( this )
-            .attr( "value" );
+function sortByPrice () {
+    var listProducts = [];
+
+    $( ".price-sort" ).on( "click", function() {
+		$( "#filter-results" ).empty(); //clears results from previous click
+
+        event.preventDefault(); //prevents window from reloading on click
+        
+        var priceSort = $( this ).attr( "value" );
+
+        console.log(priceSort)
+
             console.log("1st click works")
 
-            if (priceSort === "asc") {
-                console.log("ascending")
-            } else if (priceSort === "desc") {
-                console.log("descending")
-            } else {
-                console.log(priceSort)
-            }
+            // if (priceSort === "asc") {
+            //     console.log("ascending")
+            //     sortResults();
+            // } else if (priceSort === "desc") {
+            //     console.log("descending")
+            // } else {
+            //     console.log(priceSort)
+            // }
             
-
-	var listProducts = [];
-
 	var queryURL = "https://www.wirelessemporium.com/products.json";
 	$.ajax( {
 			url: queryURL,
@@ -137,10 +140,10 @@ function categoryFilter() {
 
 			for ( var i = 0; i < results.length; i++ ) {
 
-                var productsDiv = $( "<div class='item col-md-6 col-sm-6 col-xs-12 squares clearfix'>" );
-                var description = results[ i ].title;
-                var productType = results[ i ].product_type;
-                var productVariant = results[ i ].variants;
+                // var productsDiv = $( "<div class='item col-md-6 col-sm-6 col-xs-12 squares clearfix'>" );
+                // var description = results[ i ].title;
+                // var productType = results[ i ].product_type;
+                // var productVariant = results[ i ].variants;
 
 				var product = {
 					description: results[ i ].title,
@@ -161,21 +164,26 @@ function categoryFilter() {
 						childImage: variants.src
                     }
 
-                    var cat = $( "<p class='cat'>" )
-                    .text( productType );
-                var h4 = $( "<h4 class='product-info'>" )
-                    .text( description );
+
+
+            jQuery.extend(product, variantOfProduct)
+            listProducts.push( product );
+
+                //     var cat = $( "<p class='cat'>" )
+                //     .text( productType );
+                // var h4 = $( "<h4 class='product-info'>" )
+                //     .text( description );
                     
-                    var productImage = $( "<img>" );
-                    productImage.attr( "src", results[ i ].images[ 0 ].src );
-                    productImage.attr( "height", "400" );
-                    productImage.attr( "width", "400" )
-                    productImage.addClass( "products-image" );
-                    productsDiv.prepend( h4 );
-                    productsDiv.prepend( cat );
-                    productsDiv.prepend( productImage );
-                    $( "#filter-results" )
-                        .prepend( productsDiv );
+                //     var productImage = $( "<img>" );
+                //     productImage.attr( "src", results[ i ].images[ 0 ].src );
+                //     productImage.attr( "height", "400" );
+                //     productImage.attr( "width", "400" )
+                //     productImage.addClass( "products-image" );
+                //     productsDiv.prepend( h4 );
+                //     productsDiv.prepend( cat );
+                //     productsDiv.prepend( productImage );
+                //     $( "#filter-results" )
+                //         .prepend( productsDiv );
                 }
                 $( "#resultsHere" )
                     .addClass( "hidden" ); //hides the product results
@@ -185,36 +193,37 @@ function categoryFilter() {
                     .removeClass( "hidden" ); 
 
 				}
-				listProducts.push( product, variantOfProduct );
-            });
             
-            
-            
-            function sortResults( prop, asc ) {
-                listProducts = listProducts[i].moboPrice.sort( function( a, b ) {
-                    if ( asc ) {
-                        return ( a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 );
-                        console.log(a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 )
-                    } else {
-                        return ( b[ prop ] > a[ prop ] ) ? 1 : ( ( b[ prop ] < a[ prop ] ) ? -1 : 0 );
-                    }
-                    
-                } );
-                if (priceSort === asc ) {
-                    sortResults( prop, true) 
-                        console.log("test");
+            console.log(listProducts)
 
-                    } else {
-                    sortResults( prop, false) 
-                        console.log("test 2");
-                    }
-            }
+              
+            });
+
+            
+            
+            // function sortResults( prop, asc ) {
+            //     listProducts = listProducts[i].moboPrice.sort( function( a, b ) {
+            //         if ( asc ) {
+            //             return ( a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 );
+            //         } else {
+            //             return ( b[ prop ] > a[ prop ] ) ? 1 : ( ( b[ prop ] < a[ prop ] ) ? -1 : 0 );
+            //         }
+            //     } );
+            //     if (priceSort === asc ) {
+            //         sortResults( prop, true) 
+            //             console.log("test");
+
+            //         } else {
+            //         sortResults( prop, false) 
+            //             console.log("test 2");
+            //         }
+            // }
 
             
         });
 
         
-
+    }
 
 
 
@@ -222,4 +231,4 @@ function categoryFilter() {
 
 	// } )
 
-// newProductList();
+sortByPrice();
