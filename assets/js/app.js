7494f63a6aebf393abbaee22c965928f518f33e7
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-04 17:59:43 -0800

    updated sort by price

--- assets/js/app.js
+++ assets/js/app.js
@@ -105,7 +105,26 @@ function categoryFilter() {
 };
 
 
-function newProductList() {
+
+    $( ".price-sort" )
+	.on( "click", function() {
+		$( "#filter-results" )
+			.empty(); //clears results from previous click
+
+		event.preventDefault(); //prevents window from reloading on click
+		var priceSort = $( this )
+            .attr( "value" );
+            console.log("1st click works")
+
+            if (priceSort === "asc") {
+                console.log("ascending")
+            } else if (priceSort === "desc") {
+                console.log("descending")
+            } else {
+                console.log(priceSort)
+            }
+            
+
 	var listProducts = [];
 
 	var queryURL = "https://www.wirelessemporium.com/products.json";
@@ -117,6 +136,11 @@ function newProductList() {
 			var results = response.products;
 
 			for ( var i = 0; i < results.length; i++ ) {
+
+                var productsDiv = $( "<div class='item col-md-6 col-sm-6 col-xs-12 squares clearfix'>" );
+                var description = results[ i ].title;
+                var productType = results[ i ].product_type;
+                var productVariant = results[ i ].variants;
 
 				var product = {
 					description: results[ i ].title,
@@ -135,36 +159,67 @@ function newProductList() {
 						comparePrice: variants.compare_at_price,
 						childId: variants.id,
 						childImage: variants.src
-					}
+                    }
+
+                    var cat = $( "<p class='cat'>" )
+                    .text( productType );
+                var h4 = $( "<h4 class='product-info'>" )
+                    .text( description );
+                    
+                    var productImage = $( "<img>" );
+                    productImage.attr( "src", results[ i ].images[ 0 ].src );
+                    productImage.attr( "height", "400" );
+                    productImage.attr( "width", "400" )
+                    productImage.addClass( "products-image" );
+                    productsDiv.prepend( h4 );
+                    productsDiv.prepend( cat );
+                    productsDiv.prepend( productImage );
+                    $( "#filter-results" )
+                        .prepend( productsDiv );
+                }
+                $( "#resultsHere" )
+                    .addClass( "hidden" ); //hides the product results
+                $( "#filter-results" )
+                    .addClass( "hidden" );
+                $( "#price-results" )
+                    .removeClass( "hidden" ); 
 
 				}
 				listProducts.push( product, variantOfProduct );
-			}
+            });
+            
+            
+            
+            function sortResults( prop, asc ) {
+                listProducts = listProducts[i].moboPrice.sort( function( a, b ) {
+                    if ( asc ) {
+                        return ( a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 );
+                        console.log(a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 )
+                    } else {
+                        return ( b[ prop ] > a[ prop ] ) ? 1 : ( ( b[ prop ] < a[ prop ] ) ? -1 : 0 );
+                    }
+                    
+                } );
+                if (priceSort === asc ) {
+                    sortResults( prop, true) 
+                        console.log("test");
+
+                    } else {
+                    sortResults( prop, false) 
+                        console.log("test 2");
+                    }
+            }
+
+            
+        });
+
+        
 
 
-		} );
 
-}
 
-$( "#price-sort" )
-	.on( "click", function() {
-		$( "#filter-results" )
-			.empty(); //clears results from previous click
+		
 
-		event.preventDefault(); //prevents window from reloading on click
-		var priceSort = $( this )
-			.attr( "value" );
+	// } )
 
-		function sortResults( prop, asc ) {
-			listProducts = priceSort.sort( function( a, b ) {
-				if ( asc ) {
-					return ( a[ prop ] > b[ prop ] ) ? 1 : ( ( a[ prop ] < b[ prop ] ) ? -1 : 0 );
-				} else {
-					return ( b[ prop ] > a[ prop ] ) ? 1 : ( ( b[ prop ] < a[ prop ] ) ? -1 : 0 );
-				}
-			} );
-		}
-
-	} )
-
-newProductList();
+// newProductList();
